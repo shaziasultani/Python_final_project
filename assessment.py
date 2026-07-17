be4f7cfa5a704474de5c03d8e59dba62d10e3b62
shaziasultani <shazia.sultani098@gmail.com>
2026-07-17 12:14:38 +0430

    quiz, exam and project class

--- assessment.py
+++ assessment.py
@@ -19,3 +19,44 @@ class Assessment:
     def display_info(self):
         print("Title:", self.title)
         print("Max Score:", self.max_score)
+
+
+
+# quiz class, the assessment' parent class and here, using overriding too.
+
+class Quiz(Assessment):
+
+
+    def display_info(self):
+        print("Quiz:", self.title)
+        print("Max Score:", self.max_score)
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage > 85:
+            return "Great result!"
+        elif percentage > 60:
+            return "Good."
+        else:
+            return "Needs more quiz practice."
+
+# exam class
+class Exam(Assessment):
+
+    def display_info(self):
+        print("Exam:", self.title)
+        print("Max Score:", self.max_score)
+
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 55:
+            return "You passed the exam"
+
+        else:
+            return "You failed the exam"
+
+
+
